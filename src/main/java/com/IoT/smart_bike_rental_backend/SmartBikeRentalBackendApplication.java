10fc6584b0a900a84abe61061924155a4a129282
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-03-02 09:59:25 +0300

    project init

---
+++ src/main/java/com/IoT/smart_bike_rental_backend/SmartBikeRentalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.IoT.smart_bike_rental_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBikeRentalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBikeRentalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IoT/smart_bike_rental_backend/SmartBikeRentalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IoT.smart_bike_rental_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBikeRentalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
