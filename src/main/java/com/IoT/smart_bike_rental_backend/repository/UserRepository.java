0123684c64a4ade3a05e828d9e4f9aaa33e4ff0d
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-04-19 16:31:09 +0300

    adding user repo with findby email method for user auth

---
+++ src/main/java/com/IoT/smart_bike_rental_backend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.IoT.smart_bike_rental_backend.repository;
+
+import com.IoT.smart_bike_rental_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
