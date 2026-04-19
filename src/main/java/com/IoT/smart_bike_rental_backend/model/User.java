d054f9c7fcf49c968afb0caf8a7857ac91d4a7c3
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-04-19 16:28:12 +0300

    adding user entity

---
+++ src/main/java/com/IoT/smart_bike_rental_backend/model/User.java
@@ -0,0 +1,36 @@
+package com.IoT.smart_bike_rental_backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String name;
+    private String phone;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
