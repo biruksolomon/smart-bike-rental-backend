7a078a326e6661b16254933114e4e60d2550de57
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-04-30 11:05:27 +0300

    adding tokenvalidation response dto

---
+++ src/main/java/com/IoT/smart_bike_rental_backend/dto/TokenValidationResponse.java
@@ -0,0 +1,17 @@
+package com.IoT.smart_bike_rental_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TokenValidationResponse {
+    private Boolean valid;
+    private String message;
+    private Long userId;
+    private String email;
+}
